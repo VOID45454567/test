4f8ce512bdca3a08fde9ae4013f866c4ba9a831b
VOID45454567 <kirillvasilev5817@gmail.com>
2024-06-02 13:11:34 +0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-require_once 'app/database/connect.php';
-
-$connection = new mysqli($host, $user, $password, $name);
-if ($connection->connect_error) {
-    die('Connection failed:');
-}
